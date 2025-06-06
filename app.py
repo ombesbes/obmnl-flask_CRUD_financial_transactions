70e88e262c227175c0d975f0ecaf9b70e81fd1ee
Omar Besbes <ombesbes@gmail.com>
2025-06-06 06:34:43 -0400

    read task completed

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 # Import libraries
+from flask import Flask, request, url_for, redirect, render_template 
 
 # Instantiate Flask functionality
+app = Flask(__name__)
 
 # Sample data
+transactions = [
+    {'id': 1, 'date': '2023-06-01', 'amount': 100},
+    {'id': 2, 'date': '2023-06-02', 'amount': -200},
+    {'id': 3, 'date': '2023-06-03', 'amount': 300}
+]
 
-# Read operation
+# Read operation: List all transactions
+
+@app.route('/')
+def get_transactions():
+    return render_template('transactions.html', transactions=transactions)
+
+
 
 # Create operation
 
