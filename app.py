e5d712a62ee9cbd019c906d1eaae25a652ad63ba
Omar Besbes <ombesbes@gmail.com>
2025-06-06 07:29:54 -0400

    create task completed

--- app.py
+++ app.py
@@ -12,14 +12,28 @@ transactions = [
 ]
 
 # Read operation: List all transactions
-
 @app.route('/')
 def get_transactions():
     return render_template('transactions.html', transactions=transactions)
 
-
-
-# Create operation
+# Create operation: Display add transaction form
+# Route to handle the creation of a new transaction
+@app.route('/add', methods=['GET','POST'])
+def add_transaction():
+    # Check if the request method is POST (form submission)
+    if request.method == 'POST':
+        # Create a new transaction object using form field values
+        transation = {
+              'id': len(transactions)+1
+              'date': request.form['date']
+              'amount': float(request.form['amount'])
+             }
+        # append the new transaction to the transactions list
+        transactions.append(new_transaction)
+        # Redirect to the transactions list page after adding the new transaction
+        return redirect(url_for('get_transactions'))
+    # If the request method is GET, render the form template to display the add transaction form
+    return render_template('form.html')    
 
 # Update operation
 
